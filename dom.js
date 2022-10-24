319c62619ab55f61c5ad73179411120dae42f9c0
mdshayequeahmad <shayeque01@gmail.com>
2022-10-24 19:46:21 +0530

    second commit

--- dom.js
+++ dom.js
@@ -31,8 +31,18 @@
 //GET ELEMENTS BY CLASSNAME
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
 items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
 
-var element = document.getElementsByClassName('title');
-element[0].style.fontWeight = 'bold';
-element[0].style.color = '#34eb4c';
+// for (var i=0; i<items.length; i++) {
+//     items[i].style.backgroundColor = '#b5af9e';
+// }
+
+
+items[2].style.backgroundColor = 'green';
+
+for (var i=0; i<items.length; i++) {
+    items[i].style.fontWeight = 'bold';
+}
