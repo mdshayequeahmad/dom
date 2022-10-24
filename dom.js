b9f6ccdee17a4c2e1733b85fbf9011c6279ec3be
mdshayequeahmad <shayeque01@gmail.com>
2022-10-25 02:30:38 +0530

    5th commit

--- dom.js
+++ dom.js
@@ -112,10 +112,96 @@
 // }
 
 //Deliverable
-var seconditem = document.querySelector('.list-group-item:nth-child(2');
-seconditem.style.color = 'green';
+// var seconditem = document.querySelector('.list-group-item:nth-child(2');
+// seconditem.style.color = 'green';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for (var i=0; i < odd.length; i++){
-    odd[i].style.backgroundColor = 'green';
-}
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for (var i=0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = 'green';
+// }
+
+
+//TRAVERSING THE DOM
+var itemList = document.querySelector('#items');
+//parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+//parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// //childNodes
+// // console.log(itemList.childNodes);
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// //firstChild
+// console.log(itemList.firstChild);
+
+// //FirstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 5';
+
+
+//LasttChild
+// console.log(itemList.lastChild);
+
+// //LastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+//NextSibling
+// console.log(itemList.nextSibling);
+
+//NextElementSibling
+// console.log(itemList.nextElementSibling);
+
+//previousSibling
+// console.log(itemList.previousSibling);
+
+//previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+//createElement
+
+//create a div
+var newdiv = document.createElement('div');
+
+//add class
+newdiv.className = 'hello';
+
+//add id 
+newdiv.id = 'hello1';
+
+//add att
+newdiv.setAttribute('title', 'hello div')
+
+//create a text node
+var newdivtext = document.createTextNode('Hello World');
+
+//add text to div
+newdiv.appendChild(newdivtext);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newdiv);
+
+newdiv.style.fontSize = '30px';
+
+container.insertBefore(newdiv, h1);
+
+//Deliverable
+// Create a new, plain element
+var it = document.getElementById('items');
+// create a new li node
+var lis = document.createElement('li');
+lis.textContent = 'hello World';
+
+// insert a new node before the first list item
+items.insertBefore(lis, it.firstElementChild);
